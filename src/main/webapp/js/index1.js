fea2abc6a70f8a2989c2ccc9129b073cc29f5b30
sunlightcs <sunlightcs@gmail.com>
2017-02-14 13:09:51 +0800

    菜单点击修复

--- src/main/webapp/js/index1.js
+++ src/main/webapp/js/index1.js
@@ -17,14 +17,28 @@ layui.config({
 	    });
 	}).resize();
   
-$(function(){
-    $('#larry-nav-side').click(function(){
-        if($(this).attr('lay-filter')!== undefined){
-            $(this).children('ul').find('li').each(function(){
-                var $this = $(this);
-                if($this.find('dl').length > 0){
-                   var $dd = $this.find('dd').each(function(){
-                       $(this).click(function(){
+	$(function(){
+	    $('#larry-nav-side').click(function(){
+	        if($(this).attr('lay-filter')!== undefined){
+	            $(this).children('ul').find('li').each(function(){
+	                var $this = $(this);
+	                if($this.find('dl').length > 0){
+	                   var $dd = $this.find('dd').each(function(){
+	                       $(this).on('click', function() {
+	                           var $a = $(this).children('a');
+	                           var href = $a.data('url');
+	                           var icon = $a.children('i:first').data('icon');
+	                           var title = $a.children('span').text();
+	                           var data = {
+	                                 href: href,
+	                                 icon: icon,
+	                                 title: title
+	                           }
+	                           navtab.tabAdd(data);
+	                       });
+	                   });
+	                }else{
+	                	$this.on('click', function() {
                            var $a = $(this).children('a');
                            var href = $a.data('url');
                            var icon = $a.children('i:first').data('icon');
@@ -35,61 +49,45 @@ $(function(){
                                  title: title
                            }
                            navtab.tabAdd(data);
-                       });
-                   });
-                }else{
-                    $this.click(function(){
-                           var $a = $(this).children('a');
-                           var href = $a.data('url');
-                           var icon = $a.children('i:first').data('icon');
-                           var title = $a.children('span').text();
-                           var data = {
-                                 href: href,
-                                 icon: icon,
-                                 title: title
-                           }
-                           navtab.tabAdd(data);
-                    });
-                }
-            });
-        }
-    })
-})
+	                    });
+	                }
+	            });
+	        }
+	    }).trigger("click");
+	});
 });
 
 
-'use strict';
 layui.use(['jquery','layer','element'],function(){
 	window.jQuery = window.$ = layui.jquery;
 	window.layer = layui.layer;
-  var element = layui.element();
+	var element = layui.element();
   
-// larry-side-menu向左折叠
-$('.larry-side-menu').click(function() {
-  var sideWidth = $('#larry-side').width();
-  if(sideWidth === 200) {
-      $('#larry-body').animate({
-        left: '0'
-      }); 
-      $('#larry-footer').animate({
-        left: '0'
-      });
-      $('#larry-side').animate({
-        width: '0'
-      });
-  } else {
-      $('#larry-body').animate({
-        left: '200px'
-      });
-      $('#larry-footer').animate({
-        left: '200px'
-      });
-      $('#larry-side').animate({
-        width: '200px'
-      });
-  }
-});
-
+	// larry-side-menu向左折叠
+	$('.larry-side-menu').click(function() {
+	  var sideWidth = $('#larry-side').width();
+	  if(sideWidth === 200) {
+	      $('#larry-body').animate({
+	        left: '0'
+	      }); 
+	      $('#larry-footer').animate({
+	        left: '0'
+	      });
+	      $('#larry-side').animate({
+	        width: '0'
+	      });
+	  } else {
+	      $('#larry-body').animate({
+	        left: '200px'
+	      });
+	      $('#larry-footer').animate({
+	        left: '200px'
+	      });
+	      $('#larry-side').animate({
+	        width: '200px'
+	      });
+	  }
+	});
 });
 
 
