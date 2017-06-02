1d2677c02d4a606e0cdaeba120ad33c3d65f22b0
sunlightcs <sunlightcs@gmail.com>
2017-06-02 22:11:19 +0800

    Spring视图修改

--- renren-web/src/main/java/io/renren/controller/SysPageController.java
+++ renren-web/src/main/java/io/renren/controller/SysPageController.java
@@ -14,13 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SysPageController {
 	
-	@RequestMapping("sys/{url}.html")
-	public String page(@PathVariable("url") String url){
-		return "sys/" + url + ".html";
+	@RequestMapping("{first}/{second}.html")
+	public String page(@PathVariable("first") String first, @PathVariable("second") String second){
+		return first + "/" + second + ".html";
 	}
 
-	@RequestMapping("generator/{url}.html")
-	public String generator(@PathVariable("url") String url){
-		return "generator/" + url + ".html";
-	}
 }
