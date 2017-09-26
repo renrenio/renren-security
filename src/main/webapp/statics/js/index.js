d1b903591582adee5f7252749f1b8a596214c3bc
daxiong.yang <daxiong.yang@qq.com>
2017-09-26 17:39:30 +0800

    格式调整

--- src/main/webapp/statics/js/index.js
+++ src/main/webapp/statics/js/index.js
@@ -3,17 +3,17 @@ var menuItem = Vue.extend({
 	name: 'menu-item',
 	props:{item:{}},
 	template:[
-	          '<li>',
-	          '<a v-if="item.type === 0" href="javascript:;">',
-	          '<i v-if="item.icon != null" :class="item.icon"></i>',
-	          '<span>{{item.name}}</span>',
-	          '<i class="fa fa-angle-left pull-right"></i>',
-	          '</a>',
-	          '<ul v-if="item.type === 0" class="treeview-menu">',
-	          '<menu-item :item="item" v-for="item in item.list"></menu-item>',
-	          '</ul>',
-	          '<a v-if="item.type === 1" :href="\'#\'+item.url"><i v-if="item.icon != null" :class="item.icon"></i><i v-else class="fa fa-circle-o"></i> {{item.name}}</a>',
-	          '</li>'
+		'<li>',
+		'	<a v-if="item.type === 0" href="javascript:;">',
+		'		<i v-if="item.icon != null" :class="item.icon"></i>',
+		'		<span>{{item.name}}</span>',
+		'		<i class="fa fa-angle-left pull-right"></i>',
+		'	</a>',
+		'	<ul v-if="item.type === 0" class="treeview-menu">',
+		'		<menu-item :item="item" v-for="item in item.list"></menu-item>',
+		'	</ul>',
+		'	<a v-if="item.type === 1" :href="\'#\'+item.url"><i v-if="item.icon != null" :class="item.icon"></i><i v-else class="fa fa-circle-o"></i> {{item.name}}</a>',
+		'</li>'
 	].join('')
 });
 
