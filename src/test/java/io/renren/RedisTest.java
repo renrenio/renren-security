9215a41d3fe33ac4975b3d456214a75c867036c8
kikisili <chenli03200320@126.com>
2017-10-16 22:28:33 +0800

    更新测试用例

--- src/test/java/io/renren/RedisTest.java
+++ src/test/java/io/renren/RedisTest.java
@@ -1,9 +1,6 @@
 package io.renren;
 
 import io.renren.common.utils.RedisUtils;
-import io.renren.modules.sys.entity.SysUserEntity;
-import io.renren.modules.sys.service.SysUserService;
-import org.apache.commons.lang.builder.ToStringBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +13,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class RedisTest {
     @Autowired
     private RedisUtils redisUtils;
-    @Autowired
-    private SysUserService sysUserService;
 
     @Test
     public void test(){
-        SysUserEntity user = sysUserService.queryObject(1L);
-        System.out.println(ToStringBuilder.reflectionToString(user));
+        
+        redisUtils.set("domain", "renren.io");
+        String domain = redisUtils.get("domain");
 
-        redisUtils.set("user", user);
-
-        user = redisUtils.get("user", SysUserEntity.class);
         System.out.println(ToStringBuilder.reflectionToString(user));
     }
 }
