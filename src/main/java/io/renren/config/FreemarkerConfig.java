2837657628da0067d2c4db6b55078d1e09ff9f7f
sunlightcs <sunlightcs@gmail.com>
2017-10-17 17:44:51 +0800

    优化

--- src/main/java/io/renren/config/FreemarkerConfig.java
+++ src/main/java/io/renren/config/FreemarkerConfig.java
@@ -1,12 +1,16 @@
 package io.renren.config;
 
 import io.renren.modules.sys.shiro.ShiroTag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Freemarker配置
@@ -17,16 +21,33 @@ import java.util.Map;
  */
 @Configuration
 public class FreemarkerConfig {
-
+    @Autowired
+    protected freemarker.template.Configuration configuration;
     @Bean
     public FreeMarkerConfigurer freeMarkerConfigurer(ShiroTag shiroTag){
         FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
         configurer.setTemplateLoaderPath("/WEB-INF/views/");
 
-        Map<String, Object> variables = new HashMap<>();
+        Map<String, Object> variables = new HashMap<>(1);
         variables.put("shiro", shiroTag);
         configurer.setFreemarkerVariables(variables);
 
+        Properties settings = new Properties();
+        settings.setProperty("default_encoding", "utf-8");
+        settings.setProperty("number_format", "0.##");
+        configurer.setFreemarkerSettings(settings);
+
         return configurer;
+    }
+
+    @Bean
+    public FreeMarkerViewResolver getFreemarkViewResolver() {
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setCache(false);
+        freeMarkerViewResolver.setSuffix(".html");
+        freeMarkerViewResolver.setRequestContextAttribute("request");
+        freeMarkerViewResolver.setContentType("text/html; charset=UTF-8");
+        freeMarkerViewResolver.setViewClass(FreeMarkerView.class);
+        return freeMarkerViewResolver;
     }
 }
