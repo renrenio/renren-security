e86c2c4fb233a93b72ff33d2dcf73560104d9d1a
wxxiong6 <601115211@qq.com>
2019-01-30 14:08:02 +0800

    fix 页面出现滚动条问题

--- renren-admin/src/main/resources/statics/js/index.js
+++ renren-admin/src/main/resources/statics/js/index.js
@@ -26,7 +26,7 @@ var menuItem = Vue.extend({
 //iframe自适应
 $(window).on('resize', function() {
 	var $content = $('.content');
-	$content.height($(this).height() - 120);
+	$content.height($(this).height() - 154);
 	$content.find('iframe').each(function() {
 		$(this).height($content.height());
 	});
